40e35c1f4309736e559a88f4f4f3b8d3e02e6d4f
elanez <61147852+elanez@users.noreply.github.com>
2022-03-25 08:59:27 +0800

    Update DQN.py
    Added Experience replay functions
    Added load model

--- src/DQN.py
+++ src/DQN.py
@@ -1,18 +1,23 @@
 import os
+import sys
+import random
 import numpy as np
 
 from tensorflow import keras
-from keras import layers
-from keras import losses
+from keras import layers, losses
+from keras.models import load_model
 from keras.optimizers import Adam
 
 class DQN:
-    def __init__(self, num_layers, width, batch_size, learning_rate, input_dim, output_dim):
-        self._input_dim = input_dim
-        self._output_dim = output_dim
-        self._batch_size = batch_size
-        self._learning_rate = learning_rate
-        self._model = self._build_model(num_layers, width)
+    def __init__(self):
+        self.input_dim
+        self.output_dim
+        self.batch_size
+        self.learning_rate
+        self._model = self._build_model()
+        self.sample
+        self.size_max
+        self.size_min
 
     def _create_model(self, num_layers, width):
         #Create a fully connected neural network
@@ -26,5 +31,32 @@ class DQN:
         model.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learning_rate))
         return model
 
-    def save_model(self, path):
-        self._model.save(os.path.join(path, '../Models/trained_model.h5'))
+    def save_model(self):
+        self._model.save(os.path('../Models/trained_model.h5'))
+    
+    def _load_model(self):
+        model_file_path = os.path('../Models/trained_model.h5')
+        
+        if os.path.isfile(model_file_path):
+            loaded_model = load_model(model_file_path)
+            return loaded_model
+        else:
+            sys.exit("Model not found")
+
+    #EXPRIENCE REPLAY
+    def add_sample(self, sample):
+        self._samples.append(sample)
+        if self._size_now() > self._size_max:
+            self._samples.pop(0) 
+
+    def get_samples(self, n):
+        if self._size_now() < self._size_min:
+            return []
+
+        if n > self._size_now():
+            return random.sample(self._samples, self._size_now())  
+        else:
+            return random.sample(self._samples, n)
+
+    def _size_now(self):
+        return len(self._samples)
