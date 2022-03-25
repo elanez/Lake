19755d9381611255ad23f77fd46f3bab416735e4
elanez <61147852+elanez@users.noreply.github.com>
2022-03-25 14:02:25 +0800

    Update DQN.py
    -Added train functions
    -Increase code readability

--- src/DQN.py
+++ src/DQN.py
@@ -10,29 +10,29 @@ from keras.optimizers import Adam
 
 class DQN:
     def __init__(self):
-        self.input_dim
-        self.output_dim
-        self.batch_size
+        self.input_dim = 3
+        self.output_dim = 2
+        self.batch_size = 32
         self.learning_rate
-        self._model = self._build_model()
+        self._model = self._build_model(3)
         self.sample
         self.size_max
         self.size_min
 
+    '''
+    INITIALIZE MODEL
+    '''
+    
     def _create_model(self, num_layers, width):
-        #Create a fully connected neural network
         inputs = keras.Input(shape=(self._input_dim,))
         x = layers.Dense(width, activation='relu')(inputs)
         for _ in range(num_layers):
             x = layers.Dense(width, activation='relu')(x)
         outputs = layers.Dense(self._output_dim, activation='linear')(x)
 
-        model = keras.Model(inputs=inputs, outputs=outputs, name='my_model')
+        model = keras.Model(inputs=inputs, outputs=outputs, name='model')
         model.compile(loss=losses.mean_squared_error, optimizer=Adam(lr=self._learning_rate))
         return model
-
-    def save_model(self):
-        self._model.save(os.path('../Models/trained_model.h5'))
     
     def _load_model(self):
         model_file_path = os.path('../Models/trained_model.h5')
@@ -42,8 +42,24 @@ class DQN:
             return loaded_model
         else:
             sys.exit("Model not found")
+    
+    '''
+    TRAINING ARC
+    '''
 
-    #EXPRIENCE REPLAY
+    def predict_batch(self, states):
+        return self._model.predict(states)
+
+    def train_batch(self, states, q):
+        self._model.fit(states, q, epochs=1, verbose=0)
+
+    def save_model(self):
+        self._model.save(os.path('../Models/trained_model.h5'))
+    
+    '''
+    EXPRIENCE REPLAY
+    '''
+
     def add_sample(self, sample):
         self._samples.append(sample)
         if self._size_now() > self._size_max:
@@ -60,3 +76,4 @@ class DQN:
 
     def _size_now(self):
         return len(self._samples)
+    
