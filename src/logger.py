ce64a6cbdd0291bcec56ec0e05c028a3fef53a4e
elanez <61147852+elanez@users.noreply.github.com>
2022-04-15 15:13:18 +0800

    Changed logger to static

--- src/logger.py
+++ src/logger.py
@@ -6,37 +6,19 @@ in the near future
 ERROR: This tells that due to a more serious problem, the software has not been able to perform some function
 CRITICAL: his tells serious error, indicating that the program itself may be unable to continue running
 '''
+
 import logging
 
-class Logger:
-    def __init__(self, name, file_name):
-        self._logger = logging.getLogger(name)
-        self._file_handler = logging.FileHandler(file_name)
-        self._configure()
-    
-    def _configure(self):
-        self._logger.setLevel(logging.INFO)
-        self._file_handler.setLevel(logging.INFO)
+logger = logging.getLogger('logs')
+logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self._file_handler.setFormatter(formatter)
+file_handler = logging.FileHandler('debug.log')
+file_handler.setLevel(logging.DEBUG)
 
-        self._logger.addHandler(self._file_handler)
+formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
 
-    '''
-    LOG MESSAGE
-    '''
-    def log_debug(self, message):
-        self._logger.debug(message)
-    
-    def log_info(self, message):
-        self._logger.info(message)
+logger.addHandler(file_handler)
 
-    def log_warning(self, message):
-        self._logger.warning(message)
-
-    def log_error(self, message):
-        self._logger.error(message)
-
-    def log_critical(self, message):
-        self._logger.critical(message)
+def getLogger():
+    return logger
