0d1ef2ce1f6f17a0ede6619a8f8591fcf99de917
elanez <61147852+elanez@users.noreply.github.com>
2022-04-22 15:26:57 +0800

    Delete aw.py

--- src/aw.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf 
-#import os; os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-print('tensorflow version', tf.__version__)
-
- 
-x = [[3.]]
-y = [[4.]]
-print('Result: {}'.format(tf.matmul(x, y))) 
